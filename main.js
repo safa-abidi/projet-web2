ce92aa483b072ff0b9e662687b25507a6bed5625
safa-abidi <safa.abidi99@gmail.com>
2020-09-20 01:40:39 +0100

    sem4

--- main.js
+++
@@ -1,7 +0,0 @@
-//for the hamburger menu
-const navToggle = document.querySelector(".nav-toggle");
-const nav = document.querySelector(".nav");
-
-navToggle.addEventListener("click", () => {
-  nav.classList.toggle("nav--visible");
-});
